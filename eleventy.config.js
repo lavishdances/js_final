657f5a0611d2c50f6001f9bfe1ed48690a986456
lavishdances <officiallavishdances@gmail.com>
2023-12-13 01:03:05 -0500

    test

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,6 +19,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({
 		"./content/due/": "/"
 	});
+	eleventyConfig.addPassthroughCopy({
+		"./click.js": ""
+	});
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
